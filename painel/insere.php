039bdd49072e6e382577b61032a19ea4f73c2604
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-11-29 19:12:14 -0300

    Add files via upload

--- painel/insere.php
+++ painel/insere.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if((!isset($_SESSION['id']) == true) and (!isset($_SESSION['nome']) == true) and (!isset($_SESSION['email']) == true)){
+    if ((!isset($_SESSION['id']) == true) and (!isset($_SESSION['nome']) == true) and (!isset($_SESSION['email']) == true)) {
         unset($_SESSION['id']);
         unset($_SESSION['nome']);
         unset($_SESSION['email']);
@@ -13,13 +13,19 @@
     $cidade = $_POST['cidade'];
 
     $sql = "INSERT INTO cadastro
-            (nome_cliente,email_cliente,cidade)
+            (nome_cliente, email_cliente, cidade)
             VALUES
-            ('$nome_cliente','$email_cliente','$cidade')
+            ('$nome_cliente', '$email_cliente', '$cidade')
     ";
 
-    $conexao->query($sql);
+    if ($conexao->query($sql) === TRUE) {
+        $id_cliente_inserido = $conexao->insert_id;  // Obtém o ID do cliente recém-inserido
+        // Redireciona para a página de mensagem de sucesso
+        header("Location: mensagem_sucesso.php?id=$id_cliente_inserido");
+        exit(); // Certifique-se de sair para evitar que o restante do código seja executado após o redirecionamento
+    } else {
+        echo "Erro ao inserir registro: " . $conexao->error;
+    }
 
-    header('Location: index.php');
-
+    $conexao->close();
 ?>
